d84d075db77b38d8a7d894d61e8fd3186aaa6731
Headonnn <[th.couraud@gmail.com](th.couraud@gmail.com)>
2023-03-28 10:51:49 +0200

    compo2

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,7 +1,28 @@
 function PokemonCard(){
-    return <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" ></img>
-        <figcaption>"bulbasaur"</figcaption>
-    </figure>
+    const pokemon = pokemonList[0]
+    console.log(pokemon.imgSrc)
+    const src = pokemon.imgSrc
+    const name = pokemon.name
+    return src!=undefined ? 
+     <figure>
+        
+        <img src={src} ></img>
+        <figcaption>{name}</figcaption>
+    </figure> :
+      <figure>
+        
+      <p>???</p>
+      <figcaption>{name}</figcaption>
+  </figure> 
 }
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
 export default PokemonCard;
